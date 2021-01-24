89c15451196da99527e7ca1fe429a98b6f61a37f
Miss-techy <55642026+Miss-techy@users.noreply.github.com>
2021-01-24 17:06:11 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,202 +1,192 @@
 <template>
-<div class="app">
-  <header>
-    <h1>My music</h1>
-  </header>
-
+  <div id="app">
+    <header>
+      <h1>My Music</h1>
+    </header>
     <main>
       <section class="player">
-
         <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
         <div class="controls">
           <button class="prev" @click="prev">Prev</button>
           <button class="play" v-if="!isPlaying" @click="play">Play</button>
-          
           <button class="pause" v-else @click="pause">Pause</button>
           <button class="next" @click="next">Next</button>
-
-          </div>
-
+        </div>
       </section>
-
-<section class="playlist">
-  <h3>The Playlist</h3>
-<button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 
-'song playing' : 'song'"> {{song.title }} - {{song.artist}} </button>
-</section>
-
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
     </main>
-
-
-  
-
-</div>
-  
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data() {
-    return{
+  name: 'app',
+  data () {
+    return {
       current: {},
       index: 0,
       isPlaying: false,
       songs: [
         {
-          title: 'Clarity',
-          artist: 'Andy Mineo',
-          src: require('./assets/Andy Mineo_ Clarity.mp3')
+          title: 'Heaven Knows',
+          artist: 'Hillsong',
+          src: require('./assets/Hillsong-United-Heaven-Knows-from-the-Shack-Official-Video_3sB638-DxnM (1).mp3')
         },
         {
-          title: 'Light Work',
-          artist: '116',
-          src: require('./assets/Lecrae_Light Work.mp3')
+          title: 'Indescribable',
+          artist: 'Kierra Sheard ',
+          src: require('./assets/Kierra-Sheard-Indescribable_wdZViD4rxDE.mp3')
+        },
+        {
+          title: 'One Sixteen',
+          artist: 'Trip Lee ',
+          src: require('./assets/One-Sixteen-Trip-Lee-ft-KB-and-Andy-Mineo-w-Lyrics_gYCx3pPrAL4.mp3')
         }
       ],
       player: new Audio()
     }
   },
   methods: {
-
-
-    play(song) {   //play the song
-
-      if( typeof song.src == 'undefined' ) {
-        this.current == song;
-
-        this.player.src = this.current.src
+    play (song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+        this.player.src = this.current.src;
       }
       this.player.play();
-      this.player.addEventListener('ended', function() {
-        this.index++
-        
-        if(this.index > this.songs.length -1) {
-  this.index = 0;
-
-  this.current = this.songs[this.index];
-  this.play(this.current)
-}
-
-      }.bind(this))
-      this.isPlaying = true
+      this.player.addEventListener('ended', function () {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this));
+      this.isPlaying = true;
     },
-
-
-
-    pause() {         //pause the song
-
-  this.player.pause();
-  this.isPlaying = false
-},
-
-
-
-next() {      //play the next song
-
-this.index++;
-if(this.index > this.songs.length -1) {
-  this.index = 0;
-
-  this.current = this.songs[this.index];
-  this.play(this.current)
-}
-},
-
-
-
-prev() {  //play the previous song
-
-
-  this.index--;
-if(this.index < 0) {
-  this.index = this.songs.length -1;
-
-  this.current = this.songs[this.index];
-  this.play(this.current)
-
-}
-}
-
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev () {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
   },
-
-  
-
-created() {
-  this.current = this.songs[this.index];
-  this.player.src =this.current.src;
-
-}
-  
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  }
 }
 </script>
 
 <style>
-
-*{
-  margin:0;
-  padding:0;
-  box-sizing: border-box;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
-
-body{
-  font-family: sans-serif;
+body {
+	font-family: sans-serif;
 }
-header{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
-  background: #212121;
-  color: #fff;
+header {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 15px;
+	background-color: #212121;
+	color: #FFF;
 }
-
-main{
+main {
   width: 100%;
   max-width: 768px;
   margin: 0 auto;
   padding: 25px;
 }
-
-.song-title{
-  color: #53565a;
+.song-title {
+  color: #53565A;
   font-size: 32px;
   font-weight: 700;
   text-transform: uppercase;
   text-align: center;
 }
-
-.song-title span{
+.song-title span {
   font-weight: 400;
   font-style: italic;
 }
-
-.controls{
-  display:flex;
+.controls {
+  display: flex;
   justify-content: center;
-  padding:30px 15px;
+  align-items: center;
+  padding: 30px 15px;
 }
-
-button{
+button {
   appearance: none;
-  outline:none;
+  background: none;
   border: none;
-  background:none;
+  outline: none;
   cursor: pointer;
 }
-
-.play{
+button:hover {
+  opacity: 0.8;
+}
+.play, .pause {
   font-size: 20px;
   font-weight: 700;
   padding: 15px 25px;
-  margin: 0 15px;
+  margin: 0px 15px;
   border-radius: 8px;
-  color: #fff;
-  background: #cc2e5d;
+  color: #FFF;
+  background-color: #CC2E5D;
 }
-
-button:hover{
-  opacity:0.7
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #FFF;
+  background-color: #FF5858;
+}
+.playlist {
+  padding: 0px 30px;
+}
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.playlist .song:hover {
+  color: #FF5858;
+}
+.playlist .song.playing {
+  color: #FFF;
+  background-image: linear-gradient(to right, #CC2E5D, #FF5858);
 }
 </style>
